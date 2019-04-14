ef58bf8c8540488af166e418728920c9d9f28a36
Anthony Manenti <anthonyjmanenti@localhost.localdomain>
2019-04-14 02:10:32 -0400

    front end going up

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
-var http = require ("http");
-var fs = require ("fs");
- // var gmaps = require ("gmaps.js")
+var express = require('express');
+var app = express();
+var path = require('path');
 
-var PORT= 8080;
 
-var server = http.createServer(handleRequest);
-function handleRequest(req, res) {
+app.use(express.static("./public"));
 
-    // Here we use the fs package to read our index.html file
-    fs.readFile(__dirname + "/index.html", function(err, data) {
-  
-      // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
-      // an html file.
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  }
-  
-  // Starts our server
-  server.listen(PORT, function() {
-    console.log("Server is listening on PORT: " + PORT);
-  });
-  
 
-  
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + 'index.html'));
+});
+
+app.listen(8080);
