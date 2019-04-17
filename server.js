e5ac5279838ad5adfdf98ee01020cdb7b7356c11
Anthony Manenti <anthonyjmanenti@localhost.localdomain>
2019-04-16 22:03:39 -0400

    fixed some of the content not displaying, fixed routing issue which allowed url manipulation to allow unauth signin

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 var express = require('express');
+var serveStatic = require('serve-static');
 var passport = require('passport');
 var Strategy = require('passport-facebook').Strategy;
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
@@ -20,7 +21,7 @@ app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
-app.use(express.static("./views"));
+app.use(express.static("./public"));
 
 
 // Initialize Passport and restore authentication state, if any, from the
@@ -35,17 +36,12 @@ require('./controllers/Gpassport.js')
 // Define routes.
 app.get('/',
   function(req, res){
-    res.sendFile(path.join(__dirname, "views/login.html"), { user: req.user });
+    res.sendFile(path.join(__dirname, "public/login.html"), { user: req.user });
 
   });
 
 
-  // Define routes.
-app.get('/dashboard',
-  function(req, res){
-     res.sendFile(path.join(__dirname, "views/dashboard.html"), { user: req.user });
 
-});
 
 // FB ROUTES
 
@@ -58,7 +54,7 @@ app.get('/login/facebook',
     passport.authenticate('facebook',  { failureRedirect: '/login' }),
       function(req, res) {
   // Successful authentication, redirect home.
-         res.redirect('/dashboard')
+  res.sendFile(path.join(__dirname, "public/dashboard.html"), { user: req.user });
 
   });
 
@@ -74,7 +70,8 @@ app.get('/auth/google',
     passport.authenticate('google', { failureRedirect: '/login' }),
       function(req, res) {
     // Successful authentication, redirect home.
-         res.redirect('/dashboard')
+    res.sendFile(path.join(__dirname, "public/dashboard.html"), { user: req.user });
+
   });
 
 
@@ -83,7 +80,7 @@ app.get('/auth/google',
 app.get('/profile',
   require('connect-ensure-login').ensureLoggedIn(),
     function(req, res){
-      res.sendFile(path.join(__dirname, "views/dashboard.html"), { user: req.user });
+      res.sendFile(path.join(__dirname, "public/dashboard.html"), { user: req.user });
   });
 
 
